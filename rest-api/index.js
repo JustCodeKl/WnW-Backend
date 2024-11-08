d9bc57d6dfdacf690ef6761f2f0593258e25133a
Willy Wakam <williamswakam09@gmail.com>
2024-11-08 01:51:37 +0100

    added jtw_secret to .env file

--- rest-api/index.js
+++ rest-api/index.js
@@ -14,7 +14,7 @@ const Booking = require('./models/Booking.js')
 require('dotenv').config();
 
 const bcryptSalt = bcrypt.genSaltSync(10);
-const jwtSecret = 'jscbshcshssdsuegfezefbekwr3zzz23'
+const jwtSecret = process.env.JWT_SECRET
 
 app.use(express.json());
 app.use(cookieParser());
