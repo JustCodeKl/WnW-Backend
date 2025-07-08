c5db301cfefe9bb0e6d19ee564cfc1a41576c4bc
Willy Wakam <williamswakam09@gmail.com>
2025-07-08 21:28:35 +0200

    Fixed so,e bugs

--- rest-api/routes/auth/auth-routes.js
+++ rest-api/routes/auth/auth-routes.js
@@ -1,11 +1,20 @@
-
 const express = require("express");
-const { login, register, logout, authMiddleware } = require("../../controllers/auth/auth-controllers");
+const {
+  login,
+  register,
+  logout,
+  authMiddleware,
+} = require("../../controllers/auth/auth-controllers");
 const router = express.Router();
 
-router.post("/login", login)
-router.post("/register", register)
-router.post("/logout", logout)
-router.get("/profile", authMiddleware)
+router.post("/login", login);
+router.post("/register", register);
+router.post("/logout", logout);
+router.get("/profile", authMiddleware, (req, res) => {
+  return res.json({
+    success: true,
+    user: req.user, // from middleware
+  });
+});
 
 module.exports = router;
